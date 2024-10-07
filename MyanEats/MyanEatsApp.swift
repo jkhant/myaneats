8001b6113edd88d7e9dd36218494d1f45e00e85e
jkhant <j.khant@icloud.com>
2024-10-07 12:42:46 -0700

    Initial Commit

---
+++ MyanEats/MyanEatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyanEatsApp.swift
+//  MyanEats
+//
+//  Created by Kaung Min Khant on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct MyanEatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyanEatsTests/MyanEatsTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyanEatsTests.swift
+//  MyanEatsTests
+//
+//  Created by Kaung Min Khant on 10/7/24.
+//
+
+import Testing
+@testable import MyanEats
+
+struct MyanEatsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
